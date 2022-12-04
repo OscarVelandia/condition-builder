67b3e724203ad2b8cc9fdaadeb072f484febe503
Oscar <oscar.daniel.v.s.235@gmail.com>
2022-12-04 13:06:57 -0500

    Add resolvers to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@components', replacement: '/src/components' },
+      { find: '@features', replacement: '/src/features' },
+      { find: '@hooks', replacement: '/src/hooks' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@services', replacement: '/src/services' },
+      { find: '@utils', replacement: '/src/utils' },
+    ],
+  },
+});
