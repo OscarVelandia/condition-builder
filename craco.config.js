e9eb262a7d5cda9b264542173363fbb6f6f14abb
Oscar <oscar.daniel.v.s.235@gmail.com>
2022-12-18 11:01:24 -0500

    Migrate to CRA to have out of the box Jest support

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+const path = require(`path`);
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+};
