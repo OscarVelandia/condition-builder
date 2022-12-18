1a566aa79958f766c6e2f8211341912c63e9b846
Oscar <oscar.daniel.v.s.235@gmail.com>
2022-12-17 23:50:18 -0500

    Fix overflow-x in grid

---
+++ src/features/conditionBuilder/ResultDataGrid.tsx
@@ -0,0 +1,39 @@
+import { Box, Chip } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { SuccessRequestResponse } from '@services';
+
+const texts = {
+  filtered: 'Filtered',
+  total: 'Total',
+};
+
+interface Props {
+  response: SuccessRequestResponse;
+  filteredResponse: SuccessRequestResponse;
+}
+
+export function ResultDataGrid({ filteredResponse, response }: Props) {
+  const columns: Array<GridColDef> = Object.keys(response[0]).map((responseKey) => {
+    return {
+      field: responseKey,
+      width: 110,
+      headerName: responseKey,
+    };
+  });
+
+  return (
+    <>
+      <Box display="flex" gap="1rem">
+        <Chip label={`${texts.total}: ${response.length}`} />
+        <Chip
+          color="primary"
+          label={`${texts.filtered}: ${filteredResponse.length}`}
+          variant="filled"
+        />
+      </Box>
+      <Box sx={{ height: 430, width: '100%' }}>
+        <DataGrid columns={columns} rows={filteredResponse} rowsPerPageOptions={[25, 50, 100]} />
+      </Box>
+    </>
+  );
+}
