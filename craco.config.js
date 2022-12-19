11f15c1049c8d9ec878b38aa849dc5614a7117f2
Oscar <oscar.daniel.v.s.235@gmail.com>
2022-12-18 23:03:26 -0500

    Prettier fix

--- craco.config.js
+++ craco.config.js
@@ -2,13 +2,14 @@ const path = require(`path`);
 
 module.exports = {
   webpack: {
+    extensions: ['.ts'],
     alias: {
-      "@components": path.resolve(__dirname, "src/components"),
-      "@features": path.resolve(__dirname, "src/features"),
-      "@hooks": path.resolve(__dirname, "src/hooks"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@services": path.resolve(__dirname, "src/services"),
-      "@utils": path.resolve(__dirname, "src/utils"),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
     },
   },
 };
